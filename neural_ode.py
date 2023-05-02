505a25c99f7cc479a92062d3e98cabe295879d0b
damien <damienbeecroft@gmail.com>
2023-05-01 17:13:23 -0700

    Took out recurrence from the neural net, since I do not know how to evaluate an RNN at arbitrary times. I also added many comments and started implementing the adjoint and loss gradient functions.

--- neural_ode.py
+++ neural_ode.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from torchdiffeq import odeint
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -15,8 +16,7 @@ class NeuralODE(nn.Module): # Neural ODE with one hidden layer
         self.epoch = epoch # number of steps to take before backpropagating error and training
         self.ode_dim = ode_dim # dimension of the ODE
         self.hidden_dim = hidden_dim # dimension of the hidden layer
-        self.A = torch.rand((hidden_dim,ode_dim + 1),requires_grad=True) # [z_current,tstep] to hidden operator
-        self.H = torch.rand((hidden_dim,hidden_dim),requires_grad=True) # hidden to hidden operator
+        self.A = torch.rand((hidden_dim,ode_dim),requires_grad=True) # [z_current,tstep] to hidden operator
         self.B = torch.rand((ode_dim,hidden_dim),requires_grad=True) # hidden to [z_next] operator
 
     ###########################################################################################
@@ -24,12 +24,11 @@ class NeuralODE(nn.Module): # Neural ODE with one hidden layer
     # NOTE: This function only helps us learn autonomous systems
     ###########################################################################################
     # z_k           : The current state (or an approximation of it such as znn_k)
-    # hidden_old    : The hidden layer from the previous iteration of the neural ODE
     ###########################################################################################
-    def fnn(self,z_k,hidden_old):
-        hidden = nn.functional.leaky_relu(torch.matmul(self.A,z_k) + torch.matmul(self.H,hidden_old)) 
+    def fnn(self,z_k):
+        hidden = nn.functional.leaky_relu(torch.matmul(self.A,z_k)) 
         output = torch.matmul(self.B,hidden)
-        return (output,hidden)
+        return output
     
     ###########################################################################################
     # Compute the loss at the current step using the sum of the squared error
@@ -41,46 +40,55 @@ class NeuralODE(nn.Module): # Neural ODE with one hidden layer
         return ((znn_k - z_k)^2).sum() 
 
     ###########################################################################################
-    # Compute d(lambda(t))/dt = (df/dz)^T * lambda(t)
+    # Compute d(a(t))/dt = (df/dz)^T * a(t)
     ###########################################################################################
+    # a         : The Lagrange multiplier (same as the adjoint)
+    # _         : Empty slot where the time is input because we are dealing with autonomous ODEs
     # z_k       : The current state (or an approximation of it such as znn_k)
-    # l         : The Lagrange multiplier
     ###########################################################################################
-    def adjoint_ode(self,z_k,hidden,l):
-        f,_ = self.fnn(z_k,hidden)
-        dldt = f.backward(l)
-        dldt.detach()
-        return dldt
+    def adjoint_ode(self,a,_,z_k):
+        f = self.fnn(z_k)
+        f.backward(a)
+        return -z_k.grad
     
-    # def loss_gradient(self,L):
-    #     dLdtheta =    
+    ###########################################################################################
+    # dL/dtheta = int_{t_0}^{t_l} (df/dtheta)^T * a(t). This function computes the points inside
+    # the integral
+    ###########################################################################################
+    # a         : The Lagrange multiplier (same as the adjoint)
+    # z_k       : The current state (or an approximation of it such as znn_k)
+    ###########################################################################################
+    def dLdtheta_points(self,a,z_k):
+        f = self.fnn(z_k)
+        f.backward(a)
+        return ((self.A).grad,(self.B).grad)
     
     ###########################################################################################
     # Function for training the neural ODE network
     ###########################################################################################
-    # tsteps    : vector of time steps to be taken (torch tensor with shape (N,1))
+    # tsteps    : vector of times at which we have the solution (torch tensor with shape (N,1))
     # z         : vector of solutions at the the corresponding time steps (torch tensor with shape (N,1))
     ###########################################################################################
-    def train(self,tsteps,z):
+    def train(self,t,z):
         k = int(0)
         N = len(z)
+        epoch = self.epoch
+        num_epochs = N//epoch # get number of epochs
         znn = torch.zeros(N,self.ode_dim) # store all the neural net approximations of z
-        l = torch.zeros(N,self.ode_dim) # store all the adjoints
+        a = torch.zeros(N,self.ode_dim) # store all the adjoints
         znn[0] = z[0] # initialize first approximation as the initial condition
-        h_old = torch.zeros(self.hidden_dim) # set the hidden input to the zero vector for the first step
-        while (k < N):
+        for j in range(num_epochs):
             L.detach() # detach the loss function from accumulating gradient data on A, B, and H.
             L = 0 # loss accumulator
-            for _ in range(self.epoch): # accumulate loss in the epoch to then backpropagate on
-                (znn[k+1],h_old) = self.fnn(znn[k],h_old)
+            for _ in range(epoch): # accumulate loss in the epoch to then backpropagate on
+                (znn[k+1]) = self.fnn(znn[k])
                 # L += self.loss(znn[k+1],z[k+1]) # increment loss
                 k += 1
             ztemp = (znn[k]).clone().requires_grad_() # temporary variable to track loss
             losstemp = self.loss(ztemp,z[k])
             losstemp.backward() 
-            l[k,:] = ztemp.grad # This sets the adjoint at time t_k to be dL/d(z(t_k))
-            for j in range(k-1,k-self.epoch-1,-1): # NOTE: This may need to be adjusted. I am not sure that the increments are exactly right
-                l[j,:] = l[j+1,:] - 
+            a[k,:] = ztemp.grad # This sets the adjoint at time t_k to be dL/d(z(t_k))
+            a[j*epoch:(j+1)*epoch,:] = odeint(self.adjoint_ode,a[k,:],torch.flip(t[j*epoch:(j+1)*epoch],dims = [0])) # NOTE: May be wrong
 
 
             # # Naive implementation that we want to avoid.
@@ -135,6 +143,19 @@ class NeuralODE(nn.Module): # Neural ODE with one hidden layer
     #     output = torch.matmul(self.B,hidden)
     #     return (output,hidden)
 
-# I was trying to do epochs in the code, so I wanted to have an inner for loop
+# In this implementation I useed a recurrent neural net, however you can't do adaptive time stepping with this.
+# So, I decided take the recurrent part out.
+
+    # ###########################################################################################
+    # # Compute the neural network approximation of the system, [znn_{k+1}].
+    # # NOTE: This function only helps us learn autonomous systems
+    # ###########################################################################################
+    # # z_k           : The current state (or an approximation of it such as znn_k)
+    # # hidden_old    : The hidden layer from the previous iteration of the neural ODE
+    # ###########################################################################################
+    # def fnn(self,z_k,hidden_old):
+    #     hidden = nn.functional.leaky_relu(torch.matmul(self.A,z_k) + torch.matmul(self.H,hidden_old)) 
+    #     output = torch.matmul(self.B,hidden)
+    #     return (output,hidden)
             
                 
